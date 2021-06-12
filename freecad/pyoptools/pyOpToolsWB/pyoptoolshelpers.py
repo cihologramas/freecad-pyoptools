a2119807ed54a671d328b799418e17619433fe49
Ricardo <ramezquitao@cihologramas.com>
2021-06-12 16:02:56 -0500

    Fix for components inside a part
    Now it is possible to create subsystems inside a part container. The propagation will work.

--- freecad/pyoptools/pyOpToolsWB/pyoptoolshelpers.py
+++ freecad/pyoptools/pyOpToolsWB/pyoptoolshelpers.py
@@ -3,36 +3,40 @@ import FreeCAD
 from pyoptools.raytrace.system import System
 from math import radians
 
+
 def getActiveSystem():
 
     objs = FreeCAD.ActiveDocument.Objects
-    rays=[]
-    complist=[]
-    
+    rays = []
+    complist = []
+
     for obj in objs:
-        #Todos los componentes de pyoptools tienen attributo cType
-        if not hasattr(obj,"cType"):
+        # Todos los componentes de pyoptools tienen attributo cType
+        if not hasattr(obj, "cType"):
             print("Object {} not recognized by pyoptools, ignored.".format(obj.Label))
             continue
         if not obj.enabled:
             continue
-        X,Y,Z = obj.Placement.Base
-        #No entiendo el orden pero parece que funciona
-        RZ,RY,RX = obj.Placement.Rotation.toEuler()
+
+        print("Object {} recognized by pyoptools".format(obj.Label))
+
+        pla = obj.getGlobalPlacement()
+        X, Y, Z = pla.Base
+
+        # No entiendo el orden pero parece que funciona
+        RZ, RY, RX = pla.Rotation.toEuler()
 
         # Hay que buscar una mejor forma de hacer esto, es decir como no tener
         # que pasar obj en los parametros
-        e=obj.Proxy.pyoptools_repr(obj)
-        # Esto es para poder imprimir en la consola de FreeCAD
-        #try:
-        # FreeCAD.Console.PrintMessage(str(e.material) + "\n")
-        #except:
-        #    pass
-        if isinstance(e,list):
+        e = obj.Proxy.pyoptools_repr(obj)
+
+        if isinstance(e, list):
             rays.extend(e)
         else:
-            complist.append((e,(X,Y,Z),(radians(RX),radians(RY),radians(RZ)),obj.Label))
+            complist.append((e, (X, Y, Z),
+                            (radians(RX), radians(RY), radians(RZ)),
+                            obj.Label))
 
-    S=System(complist)
+    S = System(complist)
 
-    return S,rays
+    return S, rays
