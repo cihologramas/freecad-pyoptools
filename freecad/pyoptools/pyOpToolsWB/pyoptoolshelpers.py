847f5d3944989e4e6e837bb047e0ecb8419ccf3f
Ricardo <ramezquitao@cihologramas.com>
2022-10-21 17:05:58 -0500

    getActiveSystem bug
    A bug in getActiveSystem was found. The angles used to position each component in pyoptools were being obtained by the toEuler method. This was wrong. A new procedure to calculate the angles was developped.

--- freecad/pyoptools/pyOpToolsWB/pyoptoolshelpers.py
+++ freecad/pyoptools/pyOpToolsWB/pyoptoolshelpers.py
@@ -2,11 +2,16 @@
 import FreeCAD
 from pyoptools.raytrace.system import System
 from freecad.pyoptools.pyOpToolsWB.qthelpers import outputDialog
-from math import radians
+from math import radians, degrees, cos, atan2, asin, pi
+from numpy.linalg import inv
 
 
 def getActiveSystem():
+    """Return the pyoptools optical system representation.
 
+    Function that converts the representation of the optical system
+    drawn in FreeCAD, to an optical system in pyoptools.
+    """
     objs = FreeCAD.ActiveDocument.Objects
     rays = []
     complist = []
@@ -28,9 +33,48 @@ def getActiveSystem():
         pla = obj.getGlobalPlacement()
         X, Y, Z = pla.Base
 
-        # No entiendo el orden pero parece que funciona
-        RZ, RY, RX = pla.Rotation.toEuler()
+        rotmat = pla.Rotation.toMatrix()
 
+        # The idea for the solution was taken from:
+        # http://eecs.qmul.ac.uk/~gslabaugh/publications/euler.pdf
+        # and adjusted to the matrix rotation order of pyoptools.
+
+        RMA31 = rotmat.A31
+
+        # Sometimes because of numericar aproximations RMA31 can be
+        # bigger than 1 ie. 1.0000000000000002
+        if RMA31 > 1:
+            RMA31 = 1.
+
+        if RMA31 < -1:
+            RMA31 = -1.
+
+        Ry = -asin(RMA31)
+
+        Cy = cos(Ry)
+
+        # Anything bigger than  0.999999999 will be taken as 1. to avoid
+        # numerical errors in the calculation of the angles.
+        # Must check if there is a better way to solve this problem
+
+        if abs(RMA31) < 0.999999999:
+            Rx = atan2(rotmat.A32/Cy, rotmat.A33/Cy)
+            Rz = atan2(rotmat.A21/Cy, rotmat.A11/Cy)
+        else:
+            Rz = 0
+            # check for sign instead than for RMA31== -1 equality
+            if RMA31 < 0:
+                Ry = pi/2.
+                Rx = atan2(rotmat.A12, rotmat.A13)
+            else: # RMA31 == 1 branch
+                Ry = -pi/2.
+                Rx = atan2(-rotmat.A12, -rotmat.A13)
+
+        RX = degrees(Rx)
+        RY = degrees(Ry)
+        RZ = degrees(Rz)
+        ############
+        
         # Hay que buscar una mejor forma de hacer esto, es decir como no tener
         # que pasar obj en los parametros
         try:
@@ -48,7 +92,7 @@ def getActiveSystem():
                 (
                     e,
                     (X, Y, Z),
-                    (radians(RX), radians(RY), radians(RZ)),
+                    (Rx, Ry, Rz),
                     obj.Label,
                 )
             )
