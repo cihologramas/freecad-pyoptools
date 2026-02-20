9e19838fe0407c46689c119566f3e3ba3031a072
Ricardo Amézquita <rao@citech.com.co>
2026-02-20 15:43:25 -0500

    Update version to 0.0.4 and enhance version retrieval from package.xml

--- version.py
+++ version.py
@@ -1,1 +1,12 @@
-__version__ = "0.0.1"
+
+import xml.etree.ElementTree as ET
+import os
+
+def get_version():
+	pkg_path = os.path.join(os.path.dirname(__file__), "package.xml")
+	tree = ET.parse(pkg_path)
+	root = tree.getroot()
+	version_tag = root.find("{https://wiki.freecad.org/Package_Metadata}version")
+	return version_tag.text if version_tag is not None else "unknown"
+
+__version__ = get_version()
